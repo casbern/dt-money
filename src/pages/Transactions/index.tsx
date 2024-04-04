f6188ac22e0c06d1eae1ab9c810f6d52807f2a9f
Cássia <cascrisbern@gmail.com>
2024-04-04 12:56:30 +0300

    added context to share the transactions worked with reduce method to creat a new data structure to be use in the summary page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { TransactionsContainer, TransactionsTable, PriceHighLight } from "./styles";
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 
 export function Transactions() {
-  const [ transactions, setTransactions ] = useState<Transaction[]>([])
 
-  useEffect( () => {
-    fetch('http://localhost:3333/transactions')
-      .then(response => response.json() )
-      .then(data => {
-        setTransactions(data)
-      })
-
-  }, [])
-
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
