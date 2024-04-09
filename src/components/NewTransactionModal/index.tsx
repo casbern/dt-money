a4f44b770fe1c3fef5509de811cf4c7d0eca2908
Cássia <cascrisbern@gmail.com>
2024-04-09 17:28:01 +0300

    added eslint and plugin created by rocketseat to create a pattern executed eslint to find and fix code pattern added new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,39 +1,57 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from './styles';
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog'
+import {
+  Overlay,
+  Content,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 import * as z from 'zod'
-import { Controller, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  type: z.enum(['income', 'outcome'])
+  type: z.enum(['income', 'outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
-
 export function NewTransactionModal() {
-  const { 
+  const { createTransaction } = useContext(TransactionsContext)
+
+  const {
     control,
-    register, 
+    register,
     handleSubmit,
-    formState: { isSubmitting }
+    formState: { isSubmitting },
+    reset,
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     defaultValues: {
       type: 'income',
-    }
+    },
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    // await new Promise(resolve => setTimeout(resolve, 2000))
 
+    const { description, price, category, type } = data
 
-    console.log(data)
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    })
+
+    reset()
   }
 
   return (
@@ -42,53 +60,56 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
         <CloseButton>
-          <X size={24}/>
+          <X size={24} />
         </CloseButton>
 
-        <form onSubmit={ handleSubmit(handleCreateNewTransaction) }>
-          <input 
-            type="text" 
-            placeholder='Descrição' 
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
             required
-            { ...register('description')}
+            {...register('description')}
           />
-          <input 
-            type="number" 
-            placeholder='Preço' 
+          <input
+            type="number"
+            placeholder="Preço"
             required
-            { ...register('price', { valueAsNumber: true })}
+            {...register('price', { valueAsNumber: true })}
           />
-          <input 
-            type="text" 
-            placeholder='Categoria' 
+          <input
+            type="text"
+            placeholder="Categoria"
             required
-            { ...register('category')}
+            {...register('category')}
           />
 
-          
-          <Controller 
+          <Controller
             control={control}
             name="type"
-            render={ ({ field }) => {
+            render={({ field }) => {
               return (
-                <TransactionType onValueChange={field.onChange} value={field.value}>
-                <TransactionTypeButton variant='income' value='income'>
-                  <ArrowCircleUp size={24}/>
-                  Entrada
-                </TransactionTypeButton>
-    
-                <TransactionTypeButton variant='outcome' value='outcome'>
-                  <ArrowCircleDown size={24}/>
-                  Saída
-                </TransactionTypeButton>
-              </TransactionType>
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
               )
             }}
           />
 
-          <button type='submit' disabled={isSubmitting}>Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
-
       </Content>
     </Dialog.Portal>
   )
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,11 @@
-import { MagnifyingGlass } from "phosphor-react";
-import { SearchFormContainer } from "./style";
-import { useForm } from "react-hook-form";
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './style'
+import { useForm } from 'react-hook-form'
 
 import * as z from 'zod'
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
-import { TransactionsContext } from "../../../../contexts/TransactionsContext";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -16,33 +16,30 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>
 export function SearchForm() {
   const { fetchTransactions } = useContext(TransactionsContext)
 
-  const { 
-    register, 
+  const {
+    register,
     handleSubmit,
-    formState: {
-      isSubmitting,
-    }
+    formState: { isSubmitting },
   } = useForm<SearchFormInputs>({
-    resolver: zodResolver(searchFormSchema)
+    resolver: zodResolver(searchFormSchema),
   })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-
     // await new Promise(resolve => setTimeout(resolve, 2000))
 
     await fetchTransactions(data.query)
   }
-  
+
   return (
-    <SearchFormContainer onSubmit={ handleSubmit(handleSearchTransactions) }>
-      <input 
-        type="text" 
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
         placeholder="Busque por transações"
-        { ...register('query') }
+        {...register('query')}
       />
 
       <button type="submit" disabled={isSubmitting}>
-        <MagnifyingGlass size={20}/>
+        <MagnifyingGlass size={20} />
         Buscar
       </button>
     </SearchFormContainer>
